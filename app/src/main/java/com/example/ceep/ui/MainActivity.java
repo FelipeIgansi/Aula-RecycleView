ef4ab48c85054270b7ba7d8b0cdab78228d6524b
FelipeIgansi <lipeigansi@gmail.com>
2022-10-18 15:12:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ceep/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ceep.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ceep.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
